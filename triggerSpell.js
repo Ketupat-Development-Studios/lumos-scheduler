2af085acbe3e5afcb6d4ba83e748c37d1b386edb
jiahai <fengjiahai@gmail.com>
2018-02-24 22:14:40 +0800

    hooked to python

--- triggerSpell.js
+++ triggerSpell.js
@@ -1,3 +1,10 @@
+let request = require('request')
 module.exports.triggerSpell = function(spellId){
     console.log(`triggered spell ${spellId}!!`)
+    request.post({
+        url: `http://0.0.0.0:5000/spells/${spellId}/pullTrigger`,
+        headers: {'content-type': 'application/json'}
+    }, (error, response, body) => {
+        console.log(error, response, body)
+    })
 }
